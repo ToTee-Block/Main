24d44732b0eab4062a29418da9de1d968ba6ec74
jinainkorea <kimjina1152@gmail.com>
2024-11-13 13:06:50 +0900

    NotBlank & yml 수정

--- src/main/java/com/example/Main/domain/Member/request/MemberCreate.java
+++ src/main/java/com/example/Main/domain/Member/request/MemberCreate.java
@@ -4,6 +4,7 @@ import com.example.Main.domain.Member.enums.MemberGender;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -12,8 +13,8 @@ public class MemberCreate {
     private String username;
     @NotBlank
     private String password;
-    @NotBlank
+    @NotNull
     private LocalDateTime birthDate;    // 예시형식: "2024-11-12T14:30:00"
-    @NotBlank
+    @NotNull
     private MemberGender gender;    // 예시형식: "M"
 }
