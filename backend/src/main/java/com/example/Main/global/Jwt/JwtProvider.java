dfff7d4fd00abc49cb2789008bef4795e0751555
jinainkorea <157002743+jinainkorea@users.noreply.github.com>
2024-11-27 09:22:46 +0900

    회원 / 예외처리, JWT & SpringSecurity 같이 사용하기 / 김진아
    * 일부 리팩토링, 쿠키 예외처리, 로그인 정보 이용시 예외처리
    
    * JWT와 SpringSecurity 같이 사용하기
    
    * 불필요한 파일 삭제
    
    admin 불포함 시키기
    
    검사자 : 박승수, 이상수, 이은철

--- backend/src/main/java/com/example/Main/global/Jwt/JwtProvider.java
+++ backend/src/main/java/com/example/Main/global/Jwt/JwtProvider.java
@@ -20,6 +20,7 @@ public class JwtProvider {
     private String secretKeyOrigin;
 
     private SecretKey cachedSecretKey;
+    private static final int A_DAY = 60 * 60 * 24;
 
     public SecretKey getSecretKey() {
         if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
@@ -33,19 +34,25 @@ public class JwtProvider {
     }
 
     public String genRefreshToken(Member member) {
-        return genToken(member, 60 * 60 * 24 * 365 * 1);
+        return genToken(member, A_DAY);
     }
 
     public String genAccessToken(Member member) {
-        return genToken(member, 5);
+        return genToken(member, A_DAY);
     }
 
 
     public String genToken (Member member, int seconds) {
         Map<String, Object> claims = new HashMap<>();
+        /*
+            토큰에 ID, EMAIL, ROLE 등록
+            토큰에 저장될 사용자 정보는 키-값 쌍을 저장하는 Map으로 저장됨
 
+            ROLE: 스프링 시큐리티의 권한 인가(Authorization)는 "ROLE_"의 접두사를 자동으로 덧붙이므로, 추가해준다.
+         */
         claims.put("id", member.getId());
         claims.put("email", member.getEmail());
+        claims.put("rules", "ROLE_" + member.getRole());
 
         long now = new Date().getTime();
         Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
@@ -61,7 +68,7 @@ public class JwtProvider {
         try {
             Jwts.parserBuilder()
                     .setSigningKey(getSecretKey())
-                    .setAllowedClockSkewSeconds(60 * 60 * 24)  // 하루 24h 내에 발급된 토큰 한정
+                    .setAllowedClockSkewSeconds(A_DAY)  // 하루 24h 내에 발급된 토큰 한정
                     .build()
                     .parseClaimsJws(token);
         } catch (Exception e) {
@@ -74,7 +81,7 @@ public class JwtProvider {
     public Map<String, Object> getClaims(String token) {
         String body = Jwts.parserBuilder()
                 .setSigningKey(getSecretKey())
-                .setAllowedClockSkewSeconds(60 * 60 * 24)  //허용된 시간 차이(스큐) 설정 (24시간)
+                .setAllowedClockSkewSeconds(A_DAY)  //허용된 시간 차이(스큐) 설정 (24시간)
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
