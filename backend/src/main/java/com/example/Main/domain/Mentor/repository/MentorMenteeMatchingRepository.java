1fb9f6eb1989feed84a9171bced51bf0aff2e0d3
jinainkorea <157002743+jinainkorea@users.noreply.github.com>
2024-12-05 21:46:37 +0900

    멘토 / 멘토링 목록, 승인, 거절 / 김진아 (#33)
    * 멘토 / 멘토링 목록, 승인, 거절 / 김진아
    
    * 추가 수정
    
    * 멘토의 멘토링 신청 목록 수정

--- backend/src/main/java/com/example/Main/domain/Mentor/repository/MentorMenteeMatchingRepository.java
+++ backend/src/main/java/com/example/Main/domain/Mentor/repository/MentorMenteeMatchingRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 public interface MentorMenteeMatchingRepository extends JpaRepository<MentorMenteeMatching, Long> {
     List<MentorMenteeMatching> findByMentor(Mentor mentor);
+
+    List<MentorMenteeMatching> findByMentorAndApprovedTrue(Mentor mentor);
 }
