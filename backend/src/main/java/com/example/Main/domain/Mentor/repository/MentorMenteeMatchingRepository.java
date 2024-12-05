39b68508a754c80c001087f611bc2a0e3ddc7212
jinainkorea <kimjina1152@gmail.com>
2024-12-05 10:09:19 +0900

    회원 / 멘토링 신청 목록, 매칭 끊기 / 김진아

--- backend/src/main/java/com/example/Main/domain/Mentor/repository/MentorMenteeMatchingRepository.java
+++ backend/src/main/java/com/example/Main/domain/Mentor/repository/MentorMenteeMatchingRepository.java
@@ -1,5 +1,6 @@
 package com.example.Main.domain.Mentor.repository;
 
+import com.example.Main.domain.Member.entity.Member;
 import com.example.Main.domain.Mentor.entity.Mentor;
 import com.example.Main.domain.Mentor.entity.MentorMenteeMatching;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,10 @@ public interface MentorMenteeMatchingRepository extends JpaRepository<MentorMent
     List<MentorMenteeMatching> findByMentor(Mentor mentor);
 
     List<MentorMenteeMatching> findByMentorAndApprovedTrue(Mentor mentor);
+
+    List<MentorMenteeMatching> findByMentee(Member mentee);
+
+    List<MentorMenteeMatching> findByMenteeAndApprovedTrue(Member mentee);
+
+    List<MentorMenteeMatching> findByMenteeAndMentor(Member mentee, Mentor mentor);
 }
