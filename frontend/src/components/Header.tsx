d04fcc463d2ba784b1b7e529571401ec4582e093
anyeok <https://github.com/anyeok>
2024-12-18 09:46:21 +0900

    관리자 / (풀스택) 임시 저장 / 박승수

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React, { useState } from "react";
 import { usePathname } from "next/navigation";
@@ -35,19 +35,29 @@ const Header: React.FC = () => {
       <div className={styles.navSection}>
         <nav className={styles.nav}>
           <Link href="/about">
-            <span className={isActive("/about") ? styles.active : ""}>토티블록</span>
+            <span className={isActive("/about") ? styles.active : ""}>
+              토티블록
+            </span>
           </Link>
           <Link href="/post">
-            <span className={isActive("/post") ? styles.active : ""}>블로그</span>
+            <span className={isActive("/post") ? styles.active : ""}>
+              블로그
+            </span>
           </Link>
           <Link href="/qna">
-            <span className={isActive("/qna") ? styles.active : ""}>질문답변</span>
+            <span className={isActive("/qna") ? styles.active : ""}>
+              질문답변
+            </span>
           </Link>
           <Link href="/mentor">
-            <span className={isActive("/mentor") ? styles.active : ""}>멘토찾기</span>
+            <span className={isActive("/mentor") ? styles.active : ""}>
+              멘토찾기
+            </span>
           </Link>
           <Link href="/mentor/form">
-            <span className={isActive("/mentor/form") ? styles.active : ""}>멘토신청</span>
+            <span className={isActive("/mentor/form") ? styles.active : ""}>
+              멘토신청
+            </span>
           </Link>
         </nav>
         {isLoggedIn ? (
@@ -55,14 +65,19 @@ const Header: React.FC = () => {
             <div className={styles.adminSection}>
               <span>관리자</span>
               <div className={styles.iconWrapper}>
-                <button onClick={toggleNotifications} className={styles.iconButton}>
-                  <Image 
-                    src="/icon/mdi_bell.svg" 
-                    alt="notifications" 
-                    width={24} 
-                    height={24} 
+                <button
+                  onClick={toggleNotifications}
+                  className={styles.iconButton}
+                >
+                  <Image
+                    src="/icon/mdi_bell.svg"
+                    alt="notifications"
+                    width={24}
+                    height={24}
                   />
-                  {hasNewNotification && <div className={styles.notificationDot} />}
+                  {hasNewNotification && (
+                    <div className={styles.notificationDot} />
+                  )}
                 </button>
                 {showNotifications && (
                   <div className={styles.dropdown}>
@@ -76,41 +91,57 @@ const Header: React.FC = () => {
           ) : (
             <div className={styles.userSection}>
               <div className={styles.userInfo}>
-                <Image 
-                  src="/icon/user.svg" 
-                  alt="profile" 
-                  width={32} 
-                  height={32} 
+                <Image
+                  src="/icon/user.svg"
+                  alt="profile"
+                  width={32}
+                  height={32}
                   className={styles.userImage}
                 />
                 <span className={styles.userName}>홍길동</span>
-                <button onClick={toggleProfileMenu} className={styles.iconButton}>
-                  <Image 
-                    src="/icon/more.svg" 
-                    alt="more" 
-                    width={20} 
-                    height={20} 
+                <button
+                  onClick={toggleProfileMenu}
+                  className={styles.iconButton}
+                >
+                  <Image
+                    src="/icon/more.svg"
+                    alt="more"
+                    width={20}
+                    height={20}
                   />
                 </button>
                 {showProfileMenu && (
                   <div className={styles.dropdown}>
-                    <Link href="/profile" className={styles.dropdownItem}>My Profile</Link>
-                    <Link href="/password" className={styles.dropdownItem}>Modify Password</Link>
-                    <Link href="/blog" className={styles.dropdownItem}>My Blog</Link>
-                    <Link href="/qna" className={styles.dropdownItem}>My QnA</Link>
+                    <Link href="/profile" className={styles.dropdownItem}>
+                      My Profile
+                    </Link>
+                    <Link href="/password" className={styles.dropdownItem}>
+                      Modify Password
+                    </Link>
+                    <Link href="/blog" className={styles.dropdownItem}>
+                      My Blog
+                    </Link>
+                    <Link href="/qna" className={styles.dropdownItem}>
+                      My QnA
+                    </Link>
                     <button className={styles.dropdownItem}>Logout</button>
                   </div>
                 )}
               </div>
               <div className={styles.iconWrapper}>
-                <button onClick={toggleNotifications} className={styles.iconButton}>
-                  <Image 
-                    src="/icon/mdi_bell.svg" 
-                    alt="notifications" 
-                    width={24} 
-                    height={24} 
+                <button
+                  onClick={toggleNotifications}
+                  className={styles.iconButton}
+                >
+                  <Image
+                    src="/icon/mdi_bell.svg"
+                    alt="notifications"
+                    width={24}
+                    height={24}
                   />
-                  {hasNewNotification && <div className={styles.notificationDot} />}
+                  {hasNewNotification && (
+                    <div className={styles.notificationDot} />
+                  )}
                 </button>
                 {showNotifications && (
                   <div className={styles.dropdown}>
