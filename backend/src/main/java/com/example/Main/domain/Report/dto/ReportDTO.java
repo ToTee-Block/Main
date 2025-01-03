d41cababd36d4f14b2b64ba7284cef226ccd6e71
anyeok <anyeok2@naver.com>
2025-01-03 12:04:13 +0900

    관리자 / 신고 페이지 구현 / 박승수
    * 관리자 / 신고글 관리 / 박승수
    
    * 관리자 / 중간 저장 / 박승수
    
    * 리드미 / 중간 저장 / 박승수
    
    * 리드미 / 중간 저장 / 박승수
    
    * 관리자 / 중간 저장 / 박승수
    
    검사자 : 김진아, 유윤하

--- backend/src/main/java/com/example/Main/domain/Report/dto/ReportDTO.java
+++ backend/src/main/java/com/example/Main/domain/Report/dto/ReportDTO.java
@@ -4,6 +4,8 @@ import com.example.Main.domain.Report.dto.summary.SummaryDTO;
 import com.example.Main.domain.Report.entity.Report;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ReportDTO {
 
@@ -12,12 +14,14 @@ public class ReportDTO {
     private final Object target;
     private final String reason;
     private final String status;
+    private final LocalDateTime createdDate;
 
     public ReportDTO(Report report) {
         this.reportId = report.getId();
         this.reporterName = report.getReporter() != null ? report.getReporter().getName() : "Unknown";
         this.reason = report.getReason() != null ? report.getReason().getDescription() : null;
         this.status = report.getStatus() != null ? report.getStatus().getStatus() : null;
+        this.createdDate = report.getCreatedDate();
 
         if (report.getPost() != null) {
             this.target = new SummaryDTO(report.getPost());
